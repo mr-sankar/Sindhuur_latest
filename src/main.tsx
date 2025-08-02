f65afcb53132e7be3a434657dd28d2910c9e8e10
JaveedAhamadShaik <javeedahamadshaik@gmail.com>
2025-08-02 15:08:24 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ Bootstrap CSS
 import 'react-toastify/dist/ReactToastify.css'; // ✅ Toastify CSS
 
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
